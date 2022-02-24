d6f895e5aa5b1118bbd8c0164293f7162a7343a8
lizzykim <kjdayoung@gmail.com>
2022-02-24 11:43:36 +0900

    Study promise

--- async/callback-to-promise.js
+++ async/callback-to-promise.js
@@ -1,47 +1,39 @@
+//콜백 지옥 promise로 바꾸기
+
 class UserStorage {
-  loginUser(id, password, onSuccess, onError) {
-    setTimeout(() => {
-      if (
-        (id === 'ellie' && password === 'dream') ||
-        (id === 'coder' && password === 'academy')
-      ) {
-        onSuccess(id); //id,pw 일치하면 onSuccess 실행(인자로 id 전달)
-      } else {
-        onError(new Error('not Found'));
-      }
-    }, 2000);
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === 'ellie' && password === 'dream') ||
+          (id === 'coder' && password === 'academy')
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error('not Found'));
+        }
+      }, 2000);
+    });
   }
-  getRoles(user, onSuccess, onError) {
-    setTimeout(() => {
-      if (user === 'ellie') {
-        onSuccess({ name: 'ellie', role: 'admin' });
-      } else {
-        onError(new Error('no access'));
-      }
-    }, 1000);
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === 'ellie') {
+          resolve({ name: 'ellie', role: 'admin' });
+        } else {
+          reject(new Error('no access'));
+        }
+      }, 1000);
+    });
   }
 }
 
 const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
-userStorage.loginUser(
-  id,
-  password,
-  (user) => {
-    userStorage.getRoles(
-      user,
-      (userWithRole) => {
-        alert(
-          `Hello ${userWithRole.name}, you have a ${userWithRole.role} role`
-        );
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  },
-  (error) => {
-    console.log(error);
-  }
-);
+
+userStorage
+  .loginUser(id, password)
+  .then(userStorage.getRoles) //이것이 잘 되면은!!
+  .then((user) => alert(`Hello ${user.name}, you have a ${user.role} role`)) //사용자의 역할을 잘 받아온다면
+  .catch(console.log);
